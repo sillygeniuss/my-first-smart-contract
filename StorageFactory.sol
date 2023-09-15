58d23762374d2163ffb988f9b1e8e40dd588a38d
sillygeniuss <sillygenius@outlook.com>
2023-09-15 17:40:23 +0800

    section 5: use `ABI`

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -6,9 +6,28 @@ import {SimpleStorage} from "./SimpleStorage.sol";
 import {SimpoleStorage2, SimpoleStorage3} from "./SimpleStorage.sol";
 
 contract StorageFactory {
-    SimpleStorage public simpleStorage;
+    // SimpleStorage public simpleStorage;
+    SimpleStorage[] public listOfSimpleStorage;
 
     function createSimpleStorageContract() public {
-        simpleStorage = new SimpleStorage();
+        listOfSimpleStorage.push(new SimpleStorage());
+    }
+
+    function sfStore(
+        uint256 _simpleStorgeIndex,
+        uint256 _newSimpleStorageNum
+    ) public {
+        // address(listOfSimpleStorage[_simpleStorgeIndex]).store(_newSimpleStorageNum);
+        SimpleStorage(address(listOfSimpleStorage[_simpleStorgeIndex])).store(
+            _newSimpleStorageNum
+        );
+    }
+
+    function sfRetreive(
+        uint256 _simpleStorageIndex
+    ) public view returns (uint256) {
+        return
+            SimpleStorage(address(listOfSimpleStorage[_simpleStorageIndex]))
+                .retreive();
     }
 }
