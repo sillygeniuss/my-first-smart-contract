71fac36a78985ddd0e379e2f315f9e6ffc1dc295
sillygeniuss <sillygenius@outlook.com>
2023-09-16 21:53:38 +0800

    section 8: interface & import from Github | NPM

--- smallDBank/FundMe.sol
+++ smallDBank/FundMe.sol
@@ -1,8 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
     function fund() public payable {
         require(msg.value >= 1e18, "didn't send enough money");
+    }
+
+    function getVersion() public view returns (uint256) {
+        return
+            AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306)
+                .version();
     }
 }
